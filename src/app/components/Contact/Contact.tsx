fd919bad32029ce8a2d3bf0697aafb8a2c819a83
Miguel Costa <maferncosta@gmail.com>
2026-04-16 07:59:53 +0200

    Added gitlab to contact section

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { FaLinkedinIn, FaGithub, FaFreeCodeCamp } from "react-icons/fa";
+import { RiGitlabFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
 
 const contacts = [
     { name: "LinkedIn", link: "https://www.linkedin.com/in/miguel-costa-0b8611298/", icon: { component: <FaLinkedinIn />, style: "text-blue-600 hover:text-blue-700" } },
     { name: "Email", link: "mailto:maferncosta@gmail.com", icon: { component: <MdEmail />, style: "text-red-600 hover:text-red-700" } },
     /*{ name: "Phone", link: "", icon: { component: <MdPhone />, style: "text-green-600 hover:text-green-700" } },*/
+    {name: "Gitlab", link: "https://gitlab.com/maferncosta", icon: {component: <RiGitlabFill />,  style: "text-gray-800 hover:text-gray-600" }},
     { name: "GitHub", link: "https://github.com/MAFernCosta", icon: { component: <FaGithub />, style: "text-gray-800 hover:text-gray-600" } },
     { name: "freeCodeCamp", link: "https://www.freecodecamp.org/fcc540b9a6b-1608-4da1-9f53-5f6c2432016e", icon: { component: <FaFreeCodeCamp />, style: "text-yellow-500 hover:text-yellow-600" } },
+    
 ]
 export default function Contact() {
 
